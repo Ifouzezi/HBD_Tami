a1d78eec03932e9ee4e692160d95bcd12a25d357
ifouzezi <ifoghaleuzezi@gmail.com>
2024-10-25 12:54:53 +0100

    Day 1

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple welcome animation - Add additional animations here if desired
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content');
+    content.classList.add('fade-in');
+  });
